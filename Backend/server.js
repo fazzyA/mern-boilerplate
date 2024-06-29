2603a72ae7671393a11cc35e819ce9ba999b93a8
fazzyA <faz.pak@gmail.com>
2024-06-29 23:21:30 +0500

    change for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -19,6 +19,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/",IndexRoute)
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/test", (req, res) => res.send({status: 200, message: "ok"}))
 app.use(customErrorHandler)
 
